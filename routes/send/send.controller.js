65a799eafd8d0d107e95be2095ace97a01d4878e
smcmurtry <smcmurtry@gmail.com>
2019-11-15 15:32:11 -0700

    more header setting

--- routes/send/send.controller.js
+++ routes/send/send.controller.js
@@ -6,7 +6,12 @@ const { Form } = require('../../db/model')
 
 const main = async (req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
-
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  }
   const body = req.body
 
   const responseSchema = new mongoose.Schema({
